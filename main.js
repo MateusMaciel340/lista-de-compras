9538041b86d791a5f5624b81c25b14eeefb05b8c
mateusmaciel340 <mateusmaciel340@gmail.com>
2023-05-17 11:26:38 -0300

    feat: métodos e escopo léxico

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ let listaDeItens = [];
 
 const form = document.getElementById('form-itens');
 const itensInput = document.getElementById('receber-item');
+const ulItens = document.getElementById('lista-de-itens');
+const ulItensComprados = document.getElementById('itens-comprados');
 
 form.addEventListener('submit', function (evento) {
     evento.preventDefault();
 
     salvarItens();
+    mostrarItens();
+    itensInput.focus();
 });
 
 function salvarItens() {
@@ -18,9 +22,56 @@ function salvarItens() {
     } else {
 
         listaDeItens.push({
-            valor: comprasItens
+            valor: comprasItens,
+            checar: false
         });
     
-        console.log(listaDeItens);
+        itensInput.value = '';
     }
+}
+
+function mostrarItens() {
+    ulItens.innerHTML = '';
+    ulItensComprados = '';
+    
+    listaDeItens.forEach((elemento, index) => {
+        if (elemento.checar) {
+            ulItensComprados.innerHTML += `
+                <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+                    <div>
+                        <input type="checkbox" checked class="is-clickable" />  
+                        <span class="itens-comprados is-size-5">${elemento.valor}</span>
+                    </div>
+                    <div>
+                        <i class="fa-solid fa-trash is-clickable deletar"></i>
+                    </div>
+                </li>
+            `;
+
+        } else {
+            ulItens.innerHTML += `
+                <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+                    <div>
+                        <input type="checkbox" class="is-clickable" />
+                        <input type="text" class="is-size-5" value="${elemento.valor}"/>
+                    </div>
+                    <div>
+                        <i class="fa-solid fa-trash is-clickable deletar"></i>
+                    </div>
+                </li>
+        `;
+
+        }
+    });
+
+    const inputChecks = document.querySelectorAll('input[type="checkbox"]');
+
+    inputChecks.forEach(i => {
+        i.addEventListener('click', (evento) => {
+            const valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value');
+            listaDeItens[valorDoElemento].checar = evento.target.checked;
+            mostrarItens();
+            console.log(listaDeItens[valorDoElemento].checar);
+        })
+    })
 }
